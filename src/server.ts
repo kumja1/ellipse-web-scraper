e428488f484fba3ed523cea2af6353fbdf035cf1
kumja1 <nwajiakumj@gmail.com>
2025-02-22 00:01:19 -0500

    upd: remove cors

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,8 @@
 import { scrapeSchools } from './scraper.js';
 
-function applyCorsHeaders(response: Response): Response {
-    const headers = new Headers(response.headers);
-    headers.set('Access-Control-Allow-Origin', '*');
-    headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    headers.set('Access-Control-Allow-Headers', 'Content-Type');
-    return new Response(response.body, {
-        status: response.status,
-        headers
-    });
-}
+
 
 async function handleRequest(req: Request) {
-    if (req.method === 'OPTIONS') {
-        applyCorsHeaders(new Response(null, { status: 204 }));
-    }
 
     let response: Response | null = null;
     const url = new URL(req.url)
@@ -33,7 +21,7 @@ async function handleRequest(req: Request) {
         }
 
     }
-    return applyCorsHeaders(response ?? new Response('Not Found', { status: 404 }));
+    return response ?? new Response('Not Found', { status: 404 });
 }
 
 Bun.serve({
